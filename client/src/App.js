66d19e6248b92f4ec25971d3bf70f3e563be1218
maulikprajapati <maulikprajapati47@gmail.com>
2020-01-14 22:14:51 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-import { Link } from "react-router-dom";
-import { Router, Switch, Route } from "react-router";
 import Fib from "./Fib";
 import OtherPage from "./OtherPage";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Link to="/">Home</Link>
+      <div className="">
+        <header className="">
           <Link to="/other">Other Page</Link>
+          <Link to="/">Home</Link>
+
         </header>
         <Switch>
-          <Route exact path="/other" component={OtherPage}></Route>
           <Route exact path="/" component={Fib}></Route>
+          <Route path="/other" component={OtherPage}></Route>
         </Switch>
       </div>
     </Router>
