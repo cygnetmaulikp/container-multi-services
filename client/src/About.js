5f53df1abea500da7ae2a749d5531b66764f4219
maulikprajapati <maulik.prajapati@cygnetinfotech.com>
2020-01-23 15:01:08 +0530

    page added

---
+++ client/src/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div>
+    About Page! <Link to="/">Go back to home</Link>
+  </div>
+);
--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Fib from "./Fib";
 import OtherPage from "./OtherPage";
+import AboutPage from "./About";
 
 function App() {
   return (
     <Router>
       <div className="">
         <header className="">
-          <Link to="/other">Other Page</Link>
           <Link to="/">Home</Link>
+          <Link to="/other">Other Page</Link>
+          <Link to="/about">About</Link>
 
         </header>
         <Switch>
           <Route exact path="/" component={Fib}></Route>
           <Route path="/other" component={OtherPage}></Route>
+          <Route path="/about" component={AboutPage}></Route>
         </Switch>
       </div>
     </Router>
